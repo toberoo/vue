9ad1f7a242e7a95b6975dc64e9b855f4e9e847af
Shehroze Khan <kidtrocious@Shehrozes-MBP-2.local>
2018-10-02 00:23:37 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        message: 'Hello World'
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
